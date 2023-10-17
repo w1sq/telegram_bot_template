22f3316c9fe7c265e4a95353380a7bc20d8f6da6
Artem Kokorev <artem.kokorev2005@yandex.ru>
2023-10-17 13:03:43 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,52 @@
+import typing
+import aiogram
+from config import Config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from db.storage import UserStorage, User
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+class TG_Bot():
+    def __init__(self, user_storage: UserStorage):
+        self._user_storage:UserStorage = user_storage
+        self._bot:aiogram.Bot = aiogram.Bot(token=Config.TGBOT_API_KEY)
+        self._storage:MemoryStorage = MemoryStorage()
+        self._dispatcher:aiogram.Dispatcher = aiogram.Dispatcher(self._bot, storage=self._storage)
+        self._create_keyboards()
+
+    async def init(self):
+        self._init_handler()
+
+    async def start(self):
+        print('Bot has started')
+        await self._dispatcher.start_polling()
+
+    async def _show_menu(self, message:aiogram.types.Message):
+        await message.answer('Добро пожаловать')
+
+    def _init_handler(self):
+        self._dispatcher.register_message_handler(self._user_middleware(self._show_menu), commands=['start', 'menu'])
+    
+    def _user_middleware(self, func:typing.Callable) -> typing.Callable:
+        async def wrapper(message:aiogram.types.Message, *args, **kwargs):
+            user = await self._user_storage.get_by_id(message.chat.id)
+            if user is None:
+                user = User(
+                    id = message.chat.id,
+                    role = User.USER
+                )
+                await self._user_storage.create(user)
+            if user.role != User.BLOCKED:
+                await func(message, user)
+        return wrapper
+    
+    def _admin_required(self, func:typing.Callable) -> typing.Callable:
+        async def wrapper(message:aiogram.types.Message, user:User, *args, **kwargs):
+            if user.role == User.ADMIN:
+                await func(message, user)
+        return wrapper
+
+    def _create_keyboards(self):
+        self._menu_keyboard_user = ReplyKeyboardMarkup(resize_keyboard=True)\
+            .row(KeyboardButton('Меню'))
---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+from db.db import DB
+from bot import TG_Bot
+from db.storage import UserStorage
+from config import Config
+import aioschedule as schedule
+
+async def init_db():
+    db = DB(host=Config.HOST, port=Config.PORT, login=Config.LOGIN, password=Config.PASSWORD, database = Config.DATABASE)
+    await db.init()
+    user_storage = UserStorage(db)
+    await user_storage.init()
+    return user_storage
+
+async def check_schedule():
+    while True:
+        await schedule.run_pending()
+        await asyncio.sleep(1)
+
+async def main():
+    user_storage = await init_db()
+    tg_bot = TG_Bot(user_storage)
+    await tg_bot.init()
+    await tg_bot.start()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(check_schedule())
+    loop.run_until_complete(main())
