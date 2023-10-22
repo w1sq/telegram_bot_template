96d3a689a6831d76bbbff974c5ed63cbd60ae4a3
Artem Kokorev <artem.kokorev2005@yandex.ru>
2023-10-23 01:17:17 +0300

    feat: formatted code + id -> user_id

--- bot.py
+++ bot.py
@@ -4,49 +4,58 @@ from config import Config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from db.storage import UserStorage, User
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 
 
-class TG_Bot():
+class TG_Bot:
     def __init__(self, user_storage: UserStorage):
-        self._user_storage:UserStorage = user_storage
-        self._bot:aiogram.Bot = aiogram.Bot(token=Config.TGBOT_API_KEY)
-        self._storage:MemoryStorage = MemoryStorage()
-        self._dispatcher:aiogram.Dispatcher = aiogram.Dispatcher(self._bot, storage=self._storage)
+        self._user_storage: UserStorage = user_storage
+        self._bot: aiogram.Bot = aiogram.Bot(token=Config.TGBOT_API_KEY)
+        self._storage: MemoryStorage = MemoryStorage()
+        self._dispatcher: aiogram.Dispatcher = aiogram.Dispatcher(
+            self._bot, storage=self._storage
+        )
         self._create_keyboards()
 
     async def init(self):
         self._init_handler()
 
     async def start(self):
-        print('Bot has started')
+        print("Bot has started")
         await self._dispatcher.start_polling()
 
-    async def _show_menu(self, message:aiogram.types.Message):
-        await message.answer('Добро пожаловать')
+    async def _show_menu(self, message: aiogram.types.Message):
+        await message.answer("Добро пожаловать")
 
     def _init_handler(self):
-        self._dispatcher.register_message_handler(self._user_middleware(self._show_menu), commands=['start', 'menu'])
-    
-    def _user_middleware(self, func:typing.Callable) -> typing.Callable:
-        async def wrapper(message:aiogram.types.Message, *args, **kwargs):
+        self._dispatcher.register_message_handler(
+            self._user_middleware(self._show_menu), commands=["start", "menu"]
+        )
+
+    def _user_middleware(self, func: typing.Callable) -> typing.Callable:
+        async def wrapper(message: aiogram.types.Message, *args, **kwargs):
             user = await self._user_storage.get_by_id(message.chat.id)
             if user is None:
-                user = User(
-                    id = message.chat.id,
-                    role = User.USER
-                )
+                user = User(id=message.chat.id, role=User.USER)
                 await self._user_storage.create(user)
             if user.role != User.BLOCKED:
                 await func(message, user)
+
         return wrapper
-    
-    def _admin_required(self, func:typing.Callable) -> typing.Callable:
-        async def wrapper(message:aiogram.types.Message, user:User, *args, **kwargs):
+
+    def _admin_required(self, func: typing.Callable) -> typing.Callable:
+        async def wrapper(message: aiogram.types.Message, user: User, *args, **kwargs):
             if user.role == User.ADMIN:
                 await func(message, user)
+
         return wrapper
 
     def _create_keyboards(self):
-        self._menu_keyboard_user = ReplyKeyboardMarkup(resize_keyboard=True)\
-            .row(KeyboardButton('Меню'))
+        self._menu_keyboard_user = ReplyKeyboardMarkup(resize_keyboard=True).row(
+            KeyboardButton("Меню")
+        )
--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from db.storage import UserStorage
 from config import Config
 import aioschedule as schedule
 
+
 async def init_db():
-    db = DB(host=Config.HOST, port=Config.PORT, login=Config.LOGIN, password=Config.PASSWORD, database = Config.DATABASE)
+    db = DB(
+        host=Config.HOST,
+        port=Config.PORT,
+        login=Config.LOGIN,
+        password=Config.PASSWORD,
+        database=Config.DATABASE,
+    )
     await db.init()
     user_storage = UserStorage(db)
     await user_storage.init()
     return user_storage
 
+
 async def check_schedule():
     while True:
         await schedule.run_pending()
         await asyncio.sleep(1)
+
 
 async def main():
     user_storage = await init_db()
@@ -23,7 +32,8 @@ async def main():
     await tg_bot.init()
     await tg_bot.start()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.create_task(check_schedule())
     loop.run_until_complete(main())
